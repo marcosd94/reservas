2ccb47dca7138d93f4ac8bf9882c9a5400f283e0
marcosd94 <mrcperalta.mp@gmail.com>
2016-05-17 15:39:50 -0400

    commit 17_05_2016

--- funcionario.py
+++ funcionario.py
@@ -1,8 +1,27 @@
 from persona import Persona
+from persistence import MiZODB
 class Funcionario(Persona):
     """docstring for Funcionario"""
     def __init__(self, nombre,apellido,documento_identidad,fecha_nacimiento,sexo,cargo,fecha_ingreso):
         Persona.__init__(self, nombre,apellido,documento_identidad,fecha_nacimiento,sexo)
         self.cargo = cargo
         self.fecha_ingreso = fecha_ingreso
-    def cargar_funcionario(self, )
+    def cargar_funcionario():
+        pass
+    def buscar_funcionario(self):
+        db = MiZODB()
+        dbroot = db.raiz
+        for key in dbroot.keys():
+            obj = dbroot[key]
+            if isinstance(obj, Funcionario):
+                print ( "Clave: ", key)
+                print ("Funcionario: ", obj.nombres,obj.apellidos)
+                print ("CI: ", obj.documento_identidad)
+                print ("Cargo: ", obj.cargo)
+                print ("\n---------------------------------"d)
+                db.close()
+    def eliminar_funcionario(self):
+        db = MiZODB()
+        dbroot = db.raiz
+        for key in dbroot.keys():
+            del dbroot[key]
--- persona.py
+++ persona.py
@@ -2,7 +2,7 @@ class Persona:
     """docstring for Persona"""
     def __init__(self, nombre,apellido,documento_identidad,fecha_nacimiento,sexo):
         self.nombres = nombre
-        self.apellidos = apellidos
+        self.apellidos = apellido
         self.documento_identidad = documento_identidad
         self.fecha_nacimiento = fecha_nacimiento
         self.sexo= sexo
